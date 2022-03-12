0a39dd4d2388d0ae6e1b8cc49d6ac6ae9cdf1771
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-03-12 14:30:01 -0600

    final

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+// by default runs after every re-render
+// cleanup function
+// second parameter
+const UseEffectBasics = () => {
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log('call useEffect');
+    if (value > 0) {
+      document.title = `New Messages(${value})`;
+    }
+  });
 
-function App() {
+  console.log('render component');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{value}</h1>
+      <button className='btn' onClick={() => setValue(value + 1)}>
+        click me
+      </button>
+    </>
   );
-}
+};
 
-export default App;
+export default UseEffectBasics
